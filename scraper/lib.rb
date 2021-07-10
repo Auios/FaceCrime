08749c4312e74ec8548c0440913702aa57684439
Auios <LoneAuios@gmail.com>
2021-07-10 12:59:47 -0400

    Update lib.rb

--- scraper/lib.rb
+++ scraper/lib.rb
@@ -3,10 +3,17 @@ require 'net/http'
 require 'json'
 
 def get_json(url)
-  sleep(0.25)
-  uri = URI.parse(url)
-  response = Net::HTTP.get_response(uri)
-  JSON.parse(response.body)
+  #sleep(0.25)
+  begin
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+  rescue
+    puts "MISSING\t#{url}"
+    result = []
+  end
+
+  result
 end
 
 def delete_file(filename)
